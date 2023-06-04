9d0e8fcb2e004544c25ea26c6ca0db74f51c0a63
Airrhead <129602952+Airrhead@users.noreply.github.com>
2023-06-04 23:38:47 +0800

    final
    final

---
+++ INDIEGAMEDEVELOPER3/js/change.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+            var imgBg=document.getElementById("imgBg");
+            imgBg.onclick=function(){
+                imgBg.src="images/职能2.png"
+            }
